f59401502dbbdee227cf705e9b11ff9437f0ef3e
thannansmu <thannan@smu.edu>
2022-10-26 14:16:24 -0500

    Added ability to load/save profile with variable

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+(app => {
+
+    class Profile {
+        constructor(name, location, bio, gender, age, lifestylePreference, propertyPreference, roomates) {
+            this.name = name
+            this.location = location
+            this.bio = bio
+            this.gender = gender
+            this.age = age
+            this.lifestylePreference = lifestylePreference
+            this.propertyPreference = propertyPreference
+            this.roomates = roomates
+        }
+    }
+
+    app.Profile = Profile;
+    var prof;
+
+})(app || (app = {}));
--- frontend/src/views/profile.js
+++ frontend/src/views/profile.js
@@ -6,6 +6,25 @@
         load() {
             if (!isLoaded) {
                 // Add stuff here for getting data from database and displaying it in p tag
+                if (app.prof != null) {
+                    var name = document.querySelector(".nameClass > p");
+                    var location = document.querySelector(".locationClass > p");
+                    var bio = document.querySelector(".bioClass > p");
+                    var gender = document.querySelector(".genderClass > p");
+                    var age = document.querySelector(".ageClass > p");
+                    var lifestylePreference = document.querySelector(".lifestyleClass > p");
+                    var propertyPreference = document.querySelector(".propertyClass > p");
+                    var roomates = document.querySelector(".roomateClass > p");
+
+                    name.innerText = app.prof.name;
+                    location.innerText = app.prof.location;
+                    bio.innerText = app.prof.bio;
+                    gender.innerText = app.prof.gender;
+                    age.innerText = app.prof.age;
+                    lifestylePreference.innerText = app.prof.lifestylePreference;
+                    propertyPreference.innerText = app.prof.propertyPreference;
+                    roomates.innerText = app.prof.roomates;
+                }
             }
 
             app._changeView("profileView");
