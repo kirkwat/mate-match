227851877f10a3ba03cff00122835652ec0101dd
thannansmu <thannan@smu.edu>
2022-10-30 23:54:52 -0500

    Added code for home view logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,4 +16,17 @@
     app.Profile = Profile;
     var prof;
 
+    const profiles = [
+        new Profile("Kyle", "Dallas", "This is a test", "Male", 24, "Quiet", "Appartment", 2),
+        new Profile("Joe", "Austin", "This is another test", "Male", 19, "Smoke-free", "Condo", 3)
+    ]
+
+    app.home = {
+        getProfiles() {
+            return profiles;
+        }
+    }
+
+
+
 })(app || (app = {}));
