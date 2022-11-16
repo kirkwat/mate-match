65195adbada2841be0e9d88186b124e8a5b96b41
deucepalmer <palmerd@smu.edu>
2022-11-15 19:03:15 -0600

    RDS info added to knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,12 +4,16 @@ module.exports = {
       debug: true,
       connection: {
         host : '127.0.0.1',
-        //host: 'localhost',
         port : 3306,
         user : 'root',
         password : 'password',
         insecureAuth: true,
         database : 'smu'
+        /*host: process.env.MYSQL_CLOUD_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_CLOUD_USER,
+        password: process.env.MYSQL_CLOUD_PASS,
+        database: process.env.MYSQL_DB*/
       }
     }
    };
