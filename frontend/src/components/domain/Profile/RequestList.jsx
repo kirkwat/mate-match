c7139cedd0da741ec4bf21a225a62f10a46d83a8
kirkwat <watson.kirk00@gmail.com>
2022-11-15 22:49:02 -0600

    updated all profile lists

--- frontend/src/components/domain/Profile/RequestList.jsx
+++ frontend/src/components/domain/Profile/RequestList.jsx
@@ -8,33 +8,33 @@ export const RequestList = ({ requests, standalone=false }) => {
                     <span className="text-secondary"> ({requests.length})</span>
             </h3>
             {requests.length === 0 ? (
-            <p className="bg-light rounded p-3">Your roommate requests from other users will appear here.</p>
+                <p className="bg-light rounded p-3">Your roommate requests from other users will appear here.</p>
             ) : (
-            <ul className="list-group">
-                {
-                    requests.map((roommate, index) =>
-                        <div key={index} className="card mb-3">
-                            <div className="card-header fs-4">
-                                {roommate.name}
-                                <span className="fs-5"> {roommate.gender === "male"?"(He/Him)":"(She/Her)"}</span>
-                                {/*update with routing*/}
-                                <a href="#" className="btn btn-primary btn-sm float-end">View Profile</a>
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text text-secondary float-end">
-                                    {roommate.city} - {roommate.age}
-                                </p>
-                                <p className="card-text col-10">{roommate.paragraph}</p>
-                                {/*update with api to accept or decline requests*/}
-                                <div class="btn-group">
-                                    <a href="#" className="btn btn-danger">Decline</a>
-                                    <a href="#" className="btn btn-success">Approve</a>
+                <ul className="list-group">
+                    {
+                        requests.map((roommate, index) =>
+                            <div key={index} className="card mb-3">
+                                <div className="card-header fs-4">
+                                    {roommate.name}
+                                    <span className="fs-5"> {roommate.gender === "male"?"(He/Him)":"(She/Her)"}</span>
+                                    {/*update with routing*/}
+                                    <a href="#" className="btn btn-primary btn-sm float-end">View Profile</a>
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text text-secondary float-end">
+                                        {roommate.city} - {roommate.age}
+                                    </p>
+                                    <p className="card-text col-10">{roommate.paragraph}</p>
+                                    {/*update with api to accept or decline requests*/}
+                                    <div class="btn-group">
+                                        <a href="#" className="btn btn-danger">Decline</a>
+                                        <a href="#" className="btn btn-success">Approve</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                }
-            </ul>
+                        )
+                    }
+                </ul>
             )}
         </div>
     </>
--- frontend/src/components/domain/Profile/RoommateList.jsx
+++ frontend/src/components/domain/Profile/RoommateList.jsx
@@ -7,26 +7,26 @@ export const RoommateList = ({ roommates, standalone=false }) => {
                     <span className="text-secondary"> ({roommates.length})</span>
             </h3>
             {roommates.length === 0 ? (
-            <p className="bg-light rounded p-3">Send a request to become roommates!</p>
+                <p className="bg-light rounded p-3">Send a request to become roommates!</p>
             ) : (
-            <ul className="list-group">
-                {
-                    roommates.map((roommate, index) =>
-                        <div key={index} className="card mb-3">
-                            <div className="card-header fs-4">
-                                {roommate.name}
-                                <span className="fs-5"> {roommate.gender === "male"?"(He/Him)":"(She/Her)"}</span>
+                <ul className="list-group">
+                    {
+                        roommates.map((roommate, index) =>
+                            <div key={index} className="card mb-3">
+                                <div className="card-header fs-4">
+                                    {roommate.name}
+                                    <span className="fs-5"> {roommate.gender === "male"?"(He/Him)":"(She/Her)"}</span>
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text text-secondary float-end">
+                                        {roommate.city} - {roommate.age}
+                                    </p>
+                                    <p className="card-text col-10">{roommate.paragraph}</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <p className="card-text text-secondary float-end">
-                                    {roommate.city} - {roommate.age}
-                                </p>
-                                <p className="card-text col-10">{roommate.paragraph}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            </ul>
+                        )
+                    }
+                </ul>
             )}
         </div>
     </>
