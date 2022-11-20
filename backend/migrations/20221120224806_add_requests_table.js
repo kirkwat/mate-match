2f0959351a3c0135dc520e6730cf4d43ea1e2b9b
deucepalmer <palmerd@smu.edu>
2022-11-20 17:20:54 -0600

    Update 20221120224806_add_requests_table.js

--- backend/migrations/20221120224806_add_requests_table.js
+++ backend/migrations/20221120224806_add_requests_table.js
@@ -1,9 +1,11 @@
 exports.up = function(knex) {
-    table.increments('id').notNullable().primary();
-    table.string('from').notNullable().references('email').inTable("users");
-    table.string('to').notNullable().references('email').inTable("users");
-    table.string('message');
-    table.boolean('accepted');
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').notNullable().primary();
+        table.string('from').notNullable().references('email').inTable("users");
+        table.string('to').notNullable().references('email').inTable("users");
+        table.string('message');
+        table.boolean('accepted');
+    });
 };
 
 exports.down = function(knex) {
