fa682425f5db92a5f3b663b6462f92675c9d8c24
kirkwat <watson.kirk00@gmail.com>
2022-11-26 03:12:55 -0600

    apply search to all users

--- frontend/src/components/domain/Dashboard/ProfileExplorer.jsx
+++ frontend/src/components/domain/Dashboard/ProfileExplorer.jsx
@@ -6,33 +6,21 @@ import { ProfileSearch, ResultList } from '../Dashboard';
 export const ProfileExplorer = () => {
     const { auth } = useAuth();
 
-    const roomies = [
-        {name: "Robert Derl", gender: "male", city: "DFW", age: 26, "Night-owl":false,"Early-bird":false,"Smoke-free":false,"Pet-friendly":false},
-        {name: "Dan Robins", gender: "male", city: "Dallas", age: 24, "Night-owl":true,"Early-bird":true,"Smoke-free":true,"Pet-friendly":true},
-        {name: "Steve Jackson", gender: "male", city: "Fort Worth", age: 22, "Night-owl":true,"Early-bird":false,"Smoke-free":true,"Pet-friendly":false},
-        {name: "Scott Dillon", gender: "female", city: "Austin", age: 21, "Night-owl":false,"Early-bird":true,"Smoke-free":false,"Pet-friendly":true},
-        {name: "David Charles", gender: "female", city: "Dallas", age: 31, "Night-owl":false,"Early-bird":false,"Smoke-free":true,"Pet-friendly":false},
-    ];
-
     const [profiles, setProfiles] = useState([]);
     const [searchResults, setSearchResults] = useState([]);
 
-    //TODO update for on click
     useEffect(() => {
-        //getProfiles(auth).then(x => setProfiles(x));
-
-        getProfiles(auth).then(x => 
-            //TODO switch
-            //setProfiles(x.filter(y => y.email !== auth.username && y.name !== null))
-            setSearchResults(x.filter(y => y.email !== auth.username && y.name !== null))
-        );
+        getProfiles(auth).then(x => {
+            setProfiles(x.filter(y => y.email !== auth.username && y.name !== null));
+            setSearchResults(x.filter(y => y.email !== auth.username && y.name !== null));
+        });
     }, []);
 
     return <>
-    {console.log(searchResults)}
+        {console.log(profiles)}
         <div className="container py-4">
             <div className="bg-light rounded p-5 pb-4 mb-4">
-                <ProfileSearch profiles={roomies} setSearchResults={setSearchResults}/>
+                <ProfileSearch profiles={profiles} setSearchResults={setSearchResults}/>
                 <ResultList results={searchResults}/>
             </div>
         </div>
