cfc9b3b682a574edad53c5652b5da08b5c55b59f
kirkwat <watson.kirk00@gmail.com>
2022-12-01 02:47:44 -0600

    add api link

--- frontend/src/api/Endpoint.js
+++ frontend/src/api/Endpoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-const ec2_url = '';
+const ec2_url = 'http://ec2-54-145-251-254.compute-1.amazonaws.com:8000';
 
 export default axios.create({
     // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
