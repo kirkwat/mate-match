894caa7fd44449f7203276803c316c7ef0a3cf5f
kirkwat <watson.kirk00@gmail.com>
2022-12-18 23:30:55 -0600

    knex fix

---
+++ backend/knexfile.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    client: "mysql",
+    debug: true,
+    connection: {
+      host: process.env.MYSQL_CLOUD_HOST,
+      port: process.env.MYSQL_PORT,
+      user: process.env.MYSQL_CLOUD_USER,
+      password: process.env.MYSQL_CLOUD_PASS,
+      insecureAuth: true,
+      database: process.env.MYSQL_DB,
+    },
+  },
+};
