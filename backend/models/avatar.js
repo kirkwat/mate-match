35e5697ceee654d7ce20b401611dc552ef7aa051
kirkwat <watson.kirk00@gmail.com>
2023-06-16 10:02:24 -0500

    perf: avatar images from cloudfront

--- backend/models/avatar.js
+++ backend/models/avatar.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 const {
   S3Client,
   PutObjectCommand,
-  GetObjectCommand,
   DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const {
+  CloudFrontClient,
+  CreateInvalidationCommand,
+} = require("@aws-sdk/client-cloudfront");
+const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 
 const USERS_TABLE = "users";
 
@@ -14,6 +17,9 @@ const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const bucketAccessKey = process.env.BUCKET_ACCESS_KEY;
 const bucketSecretAccessKey = process.env.BUCKET_SECRET_ACCESS_KEY;
+const cloudfrontDistributionId = process.env.CLOUDFRONT_DISTRIBUTION_ID;
+const cloudfrontKeyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
+const cloudfrontPrivateKey = process.env.CLOUDFRONT_PRIVATE_KEY;
 
 const s3 = new S3Client({
   credentials: {
@@ -21,6 +27,13 @@ const s3 = new S3Client({
     secretAccessKey: bucketSecretAccessKey,
   },
   region: bucketRegion,
+});
+
+const cloudfront = new CloudFrontClient({
+  credentials: {
+    accessKeyId: bucketAccessKey,
+    secretAccessKey: bucketSecretAccessKey,
+  },
 });
 
 const addAvatarImage = async (email, photoID, fileName, buffer, mimetype) => {
@@ -50,16 +63,12 @@ const addAvatarImage = async (email, photoID, fileName, buffer, mimetype) => {
 
 const getAvatarImages = async (users) => {
   for (const user of users) {
-    const params = {
-      Bucket: bucketName,
-      Key: user.photoID,
-    };
-
-    const command = new GetObjectCommand(params);
-    const signedUrl = await getSignedUrl(s3, command, {
-      expiresIn: 3600,
+    user.signedUrl = getSignedUrl({
+      url: "https://d2qm0iuktiryxq.cloudfront.net/" + user.photoID,
+      keyPairId: cloudfrontKeyPairId,
+      dateLessThan: new Date(Date.now() + 60 * 60 * 1000),
+      privateKey: cloudfrontPrivateKey,
     });
-    user.signedUrl = signedUrl;
   }
   return users;
 };
@@ -72,6 +81,20 @@ const removeAvatarImage = async (photoID) => {
 
   const command = new DeleteObjectCommand(params);
   await s3.send(command);
+
+  const invalidationParams = {
+    DistributionId: cloudfrontDistributionId,
+    InvalidationBatch: {
+      CallerReference: photoID,
+      Paths: {
+        Quantity: 1,
+        Items: [`/${photoID}`],
+      },
+    },
+  };
+
+  const invalidationCommand = new CreateInvalidationCommand(invalidationParams);
+  await cloudfront.send(invalidationCommand);
 
   const query = knex(USERS_TABLE).update({ photoID: null }).where({ photoID });
   return await query;
